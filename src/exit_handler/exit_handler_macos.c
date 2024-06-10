6281b2baf4fd9dc575e4c74b948cce13b0d3473b
RPDJF <ruipaulo.unify@outlook.fr>
2024-06-10 15:01:22 +0200

    fix compilation for macos

--- src/exit_handler/exit_handler_macos.c
+++ src/exit_handler/exit_handler_macos.c
@@ -1,7 +1,10 @@
 #include "exit_handler.h"
 
-int	secure_exit(t_minirt *minirt)
+int	secure_exit(void *data)
 {
+	t_minirt	*minirt;
+
+	minirt = (t_minirt *)data;
 	if (minirt->mlx)
 		mlx_destroy_window(minirt->mlx, minirt->win);
 	gfree(minirt);
