00270deef034edc962a34cff098201199327110f
Ilkay Yanar <ilkayyanar@Ilkays-MacBook-Pro.local>
2024-06-10 15:25:27 +0200

    fix merge

--- src/miniRT.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "miniRT.h"
-
-int	main(int ac, char **av)
-{
-}
--- src/miniRT.h
+++
@@ -1,7 +0,0 @@
-#ifndef MINIRT_H
-# define MINIRT_H
-
-#include "../libs/betterft/betterft.h"
-# include "../libs/minilibx_opengl/mlx.h"
-
-#endif
