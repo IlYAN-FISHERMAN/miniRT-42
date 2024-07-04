56b0a6c6354843d97d2fb500603d39e498bcaa61
RPDJF <ruipaulo.unify@outlook.fr>
2024-07-04 02:30:02 +0200

    refactor: Update main_test.c with window dimensions and rendering logic

--- src/main_test.c
+++ src/main_test.c
@@ -1,12 +1,16 @@
 #include "camera/image.h"
+#include "miniRT.h"
 
-int	main(void)
+float window_width = 1920;
+float window_height = 1080;
+
+int	main(int argc, char **argv)
 {
 	t_image		*image;
 	t_camera	*camera;
 	t_scene		*scene;
 
-	image = new_image(200, 40);
+	image = new_image(window_width, window_height);
 	if (!image)
 		return (1);
 	camera = new_camera(point3(5, 1, 0), vector3(0, 1, 0), vector3(0, 1, 0));
@@ -16,11 +20,27 @@ int	main(void)
 		gfree(image);
 		return (1);
 	}
-	set_fov(camera, M_PI / 4.0f, 200.0f / 40.0f);
+	set_fov(camera, M_PI / 4.0f, window_width / window_height);
 	scene = ft_lstadd(0, new_plane(point3(0, 0, 0), vector3(0, 1, 0)));
-	ft_lstpush(scene, new_sphere(point3(0, 1, 0), 2));
+	ft_lstpush(scene, new_sphere(point3(0, 2.5, 0), 2.1));
+	ft_lstpush(scene, new_sphere(point3(0, 0, -2.8), 1.5));
 	ray_trace(image, camera, scene);
-	print_image(image);
+	//print_image(image);
+	t_minirt	*minirt;
+	minirt = init_minirt(argc, argv);
+	for (int i = 0; i < window_width; i++)
+	{
+		for (int j = 0; j < window_height; j++)
+		{
+			int color = image->data[(int)(j * window_width + i)];
+			if (color == 1)
+				color = 0xFFFFFFFF;
+			else
+				color = 0x00000000;
+			mlx_pixel_put(minirt->mlx, minirt->win, i, j, color);
+		}
+	}
+	mlx_loop(minirt->mlx);
 	gfree(image->data);
 	gfree(image);
 	gfree(camera);
