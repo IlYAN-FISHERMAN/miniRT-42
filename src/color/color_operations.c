a21b6d4d66e5c2b7da6318863c51ef6a2094b98a
RPDJF <ruipaulo.unify@outlook.fr>
2024-10-09 14:44:34 +0200

    Refactor color multiplication to use bit shifting for better performance

--- src/color/color_operations.c
+++ src/color/color_operations.c
@@ -9,9 +9,9 @@ t_color	color_add(t_color c, t_color d)
 
 t_color	color_mult(t_color c, t_color d)
 {
-	return (color(byte_clamp((int)c.r * (int)d.r),
-			byte_clamp((int)c.g * (int)d.g),
-			byte_clamp((int)c.b * (int)d.b)));
+	return (color(byte_clamp((int)c.r * (int)d.r >> 8),
+			byte_clamp((int)c.g * (int)d.g >> 8),
+			byte_clamp((int)c.b * (int)d.b >> 8)));
 }
 
 t_color	color_scalar(t_color c, float scalar)
