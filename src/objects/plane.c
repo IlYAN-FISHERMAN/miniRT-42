6946da961f011efa976d1aff86cda121d6222f2d
RPDJF <ruipaulo.unify@outlook.fr>
2024-10-14 04:15:13 +0200

    Added new_plane

--- src/objects/plane.c
+++ src/objects/plane.c
@@ -1,4 +1,5 @@
 #include "plane.h"
+#include "../exit_handler/exit_handler.h"
 
 //  intersect_plane: Check if ray intersects plane
 //  @param intersect The intersection data
@@ -6,12 +7,23 @@
 //  @return true if the ray intersects the plane, false otherwise
 static t_xs_parent	intersect_plane(t_object *object, t_ray ray)
 {
-	t_xs_parent	xs;
+	t_xs_parent	xs_parent;
+	float		t;
+	t_vector3	normal;
 
-	ft_bzero(&xs, sizeof(t_xs_parent));
-	(void)object;
-	(void)ray;
-	return (xs);
+	ray = transform(ray, object->inv_transform);
+	normal = ((t_plane *)object->data)->normal;
+	xs_parent = xs();
+	if (fabs(vdot(ray.direction, normal)) < EPSILONF)
+		return (xs_parent);
+	t = -vdot(ray.origin, normal) / vdot(ray.direction, normal);
+	xs_parent.xs = galloc(sizeof(t_intersect));
+	if (!xs_parent.xs)
+		crash_exit(get_minirt(),
+			(char *[]){"miniRT", "render", 0}, "Malloc failed");
+	xs_parent.xs[0] = intersection(t, object);
+	xs_parent.count = 1;
+	return (xs_parent);
 }
 
 //  normal_at_plane: Get the normal at a point on the plane
@@ -20,13 +32,10 @@ static t_xs_parent	intersect_plane(t_object *object, t_ray ray)
 //  @return The normal at the point
 static t_vector3	normal_at_plane(t_object *object, t_point3 world_point)
 {
-	t_vector3	v;
-
-	ft_bzero(&v, sizeof(t_vector3));
-	(void)object;
 	(void)world_point;
-	return (v);
+	return (tm4mul(object->transform, ((t_plane *)object->data)->normal));
 }
+
 
 t_object	*new_plane(t_point3 origin, t_vector3 normal, t_color color)
 {
@@ -48,5 +57,8 @@ t_object	*new_plane(t_point3 origin, t_vector3 normal, t_color color)
 	*object = (t_object){.data = object->data, .mat = dfmaterial(color),
 		.transform = m4default(), .intersect = intersect_plane,
 		.type = o_plane, .normal_at = normal_at_plane};
+	object->transform = m4translation(vector3(origin.x, origin.y, origin.z));
+	object->inv_transform = m4invert(object->transform, 0);
+	object->tinv_transform = m4transpose(object->inv_transform);
 	return (object);
 }
