6a737c2a2782194197fb8f5d59c421bad506af9f
RPDJF <ruipaulo.unify@outlook.fr>
2024-10-22 19:53:44 +0200

    fix data race

--- src/camera/render_thread.c
+++ src/camera/render_thread.c
@@ -7,31 +7,6 @@ int	global_render(int *p, bool fast)
 				(get_minirt()->world.cam, p[1], p[0]), fast, MAX_REFLECT)));
 }
 
-/*void	render(void)
-{
-	t_minirt	*mrt;
-	int			p[2];
-	int			percent[2];
-
-	mrt = get_minirt();
-	mrt->is_rendering = true;
-	percent[0] = 0;
-	percent[1] = mrt->size->height * mrt->size->width;
-	p[0] = -1;
-	while (++p[0], p[0] < mrt->size->height)
-	{
-		p[1] = -1;
-		while (++p[1], p[1] < mrt->size->width)
-		{
-			if (!(++percent[0] % 100000))
-				print_percent(ft_itoa((percent[0] * 100) / percent[1]));
-			put_pixel_to_image(mrt->size->mlx_img, p[1], p[0],
-				global_render(p, true));
-		}
-	}
-	display();
-}*/
-
 static void	*thread_render(void *data_)
 {
 	t_thread_data	*data;
