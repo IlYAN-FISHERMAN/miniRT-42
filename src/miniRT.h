e2ef2cb145a3e31012b86986d912013c330b1f2b
RPDJF <ruipaulo.unify@outlook.fr>
2024-10-28 22:56:13 +0100

    removed unused prototype

--- src/miniRT.h
+++ src/miniRT.h
@@ -71,7 +71,6 @@ t_minirt	*init_minirt(t_minirt *minirt, int argc, char **argv);
 //	get_minirt: Get the minirt structure as a static variable
 //	@return The minirt structure
 t_minirt	*get_minirt(void);
-void		*memcheck(t_minirt *minirt, void *new);
 void		*init_minirt_mlx(t_minirt *minirt);
 void		help_message(t_minirt *minirt);
 
